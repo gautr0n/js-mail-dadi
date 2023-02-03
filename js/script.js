dcf3ffeb397610b2db478536ce036866541e49e5
Giorgio.Bocchio <giorgiobocchio2@gmail.com>
2023-02-03 16:51:52 +0100

    email comletato

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 const listEmail = ['email1@gmail.com', 'email2@gmail.com', 'email3@gmail.com', 'email4@gmail.com', 'email5@gmail.com', 'email6@gmail.com'];
-
 const neededEmail = prompt("inserire l'email desiderata");
 console.log(neededEmail);
 console.log(listEmail);
+
+let verification = 0;
+
+for ( let i = 0 ; i < listEmail.length ; i++) {
+    if (neededEmail === listEmail[i]){
+        verification = 1;
+        i += listEmail.length;
+    }
+};
+
+if (verification){
+    console.log("l'email è presente")
+    document.querySelector('.container .m-3').innerHTML = "l'email è presente";
+} else {
+    console.log("l'email non è presente")
+    document.querySelector('.container .m-3').innerHTML = "l'email non è presente";
+}
