0855faf6a7bcb364b931fe0342c3b5dba2e8bf76
Giorgio.Bocchio <giorgiobocchio2@gmail.com>
2023-02-03 16:32:08 +0100

    array e richiesta tramite prompt

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-console.log('working');
+const listEmail = ['email1@gmail.com', 'email2@gmail.com', 'email3@gmail.com', 'email4@gmail.com', 'email5@gmail.com', 'email6@gmail.com'];
+
+const neededEmail = prompt("inserire l'email desiderata");
+console.log(neededEmail);
+console.log(listEmail);
