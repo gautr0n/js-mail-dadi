ad24f3d1d52a938f44314cd97e9489514d6b13cb
Giorgio.Bocchio <giorgiobocchio2@gmail.com>
2023-02-03 17:06:18 +0100

    generatore numeri

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const listEmail = ['email1@gmail.com', 'email2@gmail.com', 'email3@gmail.com', 'email4@gmail.com', 'email5@gmail.com', 'email6@gmail.com'];
+/* const listEmail = ['email1@gmail.com', 'email2@gmail.com', 'email3@gmail.com', 'email4@gmail.com', 'email5@gmail.com', 'email6@gmail.com'];
 const neededEmail = prompt("inserire l'email desiderata");
 console.log(neededEmail);
 console.log(listEmail);
@@ -18,4 +18,15 @@ if (verification){
 } else {
     console.log("l'email non è presente")
     document.querySelector('.container .m-3').innerHTML = "l'email non è presente";
-}
+} */
+
+//ES dadi
+//1 generare due numeri random
+const diceResult = [1, 2];
+    for (let i = 0 ; i < 2 ; i++){
+        diceResult[i] = Math.floor(Math.random() * 6 + 1);
+        console.log(diceResult[i]);
+    }
+
+//2 confrontarli
+//3 scrivere a video il vincitore e il risultato
